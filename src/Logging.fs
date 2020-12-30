ea9f07eca67d45176ed6dc7907e035efb5f6482a
Andrew Olney <aolney@memphis.edu>
2020-12-30 12:27:37 -0600

    fixed circular references in toJson for dragEvents

--- src/Logging.fs
+++ src/Logging.fs
@@ -9,6 +9,11 @@ open Thoth.Fetch
 let inline toJson x = Encode.Auto.toString(4, x)
 let inline ofJson<'T> json = Decode.Auto.unsafeFromString<'T>(json)
 
+//Circular dependencies break Thoth autoencoding, however, it seems this only happens with SVG data we don't want
+// [<Import("stringify", from="flatted")>]
+// let flattedStringify(x: obj): string = jsNative
+
+
 type BlocklyLogEntry082720 =
     {
         schema: string
@@ -36,6 +41,14 @@ let mutable logUrl : string option = None
  
 let mutable idOption : string option = None
 
+//Filter out problematic JSON, e.g. circular references, on a case-by-base basis (vs npm flatted)
+let filterJson ( o : obj ) =
+    //on Blockly UI drag events, remove circular refs and data we don't want anyways
+    if not <| (o?object=null)  && not <| (o?object?element=null) && string(o?object?element).StartsWith("drag") then
+        o?object?newValue <- null
+        o?object?oldValue <- null
+    toJson( o )
+
 /// Log to server if we've been given a logUrl. Basically this is Express wrapping a database, but that repo is not public as of 12/29/20
 let LogToServer( logObject: obj ) = 
     match logUrl with
@@ -46,6 +59,8 @@ let LogToServer( logObject: obj ) =
                 | Some(id) -> id
                 //In a JupyterHub situation, we can use the username embedded in the url
                 | None -> Browser.Dom.window.location.href
-            do! Fetch.post( url, { username=username; json=toJson(logObject) } ) //caseStrategy = SnakeCase
+            do! Fetch.post( url, { username=username; json=filterJson(logObject) } ) //caseStrategy = SnakeCase
+            // do! Fetch.post( url, { username=username; json=flattedStringify(logObject) } ) //caseStrategy = SnakeCase
+            // do! Fetch.post( url, { username=username; json=toJson(logObject) } )
         } |> ignore
     | None -> ()
